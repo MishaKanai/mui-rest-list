0ef315f1d05f7797220ba3620efce2c318e79c6e
Misha Kanai <misha.a.kanai@gmail.com>
2020-07-26 23:44:36 +0300

    Export all components from components/index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,5 @@
 export { default as makeAdhocList } from "./makeAdhocList";
+export { default as Pagination } from "./Pagination/index";
+export { default as PaginationActions } from "./Pagination/Actions";
+import * as States from "./states";
+export { States };
